36fd866b17e27416d0f03cbcf1ee9898af0093a7
Omar Moreno <email@omarmoreno.net>
2023-09-19 08:07:29 -0700

    Loop over tree and extract record name and type.

--- app.cxx
+++ app.cxx
@@ -7,4 +7,39 @@ int main(int argc, char** argv) {
 
   rogue::interfaces::api::Bsp bsp("pyrogue.examples", "ExampleRoot");
 
+  auto nodes{bsp.nodeList()};
+
+  for (auto i{0}; i < boost::python::len(nodes); i++) {
+    std::string base_class_name{boost::python::extract<std::string>(
+		    nodes[i].attr("__class__")
+		            .attr("__bases__")[0].attr("__name__"))};
+   
+    if (base_class_name.compare("BaseCommand") == 0) {  
+      std::cout << "Found command, skipping" << std::endl; 
+      continue;
+    } else if (base_class_name.compare("BaseVariable") == 0) {
+      std::cout << "Building record." << std::endl;
+
+      std::string record_name{boost::python::extract<std::string>(nodes[i].attr("_path"))};
+      std::replace(record_name.begin(), record_name.end(), '.', ':'); 
+
+      
+      std::cout << "Record name: " << record_name << "\n";
+      
+      std::string type{boost::python::extract<std::string>(nodes[i].attr("typeStr"))}; 
+      std::cout << "Type: " << type << "\n"; 
+
+      if (type.compare("int") == 0) {
+        std::cout << "Found int type." << std::endl;
+	int value = boost::python::extract<int>(nodes[i].attr("value")); 
+	std::cout << "value: " << value << std::endl;
+      }
+
+      
+    }
+     	
+
+    //std::cout << base_class_name << std::endl;
+  }
+
 }
