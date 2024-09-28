c5250232f58898a5238b771414a1512aace81070
Omar Moreno <email@omarmoreno.net>
2024-09-27 17:37:48 -0700

    Update example to reflect changes to the API.

--- app.cxx
+++ app.cxx
@@ -1,6 +1,8 @@
 
 #include "rogue/interfaces/api/Root.h"
+#include "rogue/interfaces/api/Variable.h" 
 #include <boost/python.hpp>
+#include <boost/variant.hpp>
 
 #include <iostream>
 
@@ -10,44 +12,30 @@ int main(int argc, char** argv) {
   boost::python::object module{boost::python::import("pyrogue.examples")};
   boost::python::object root{module.attr("ExampleRoot")()}; 
 
-  rogue::interfaces::api::Root _root(root); 
+  rogue::interfaces::api::Root _root(root);
+  auto nodes{_root.getNodes()};
 
-  /*
-  rogue::interfaces::api::Bsp bsp("pyrogue.examples", "ExampleRoot");
-
-  auto nodes{bsp.nodeList()};
-
-  for (auto i{0}; i < boost::python::len(nodes); i++) {
-    std::string base_class_name{boost::python::extract<std::string>(
-		    nodes[i].attr("__class__")
-		            .attr("__bases__")[0].attr("__name__"))};
-   
-    if (base_class_name.compare("BaseCommand") == 0) {  
-      std::cout << "Found command, skipping" << std::endl; 
-      continue;
-    } else if (base_class_name.compare("BaseVariable") == 0) {
-      std::cout << "Building record." << std::endl;
-
-      std::string record_name{boost::python::extract<std::string>(nodes[i].attr("_path"))};
-      std::replace(record_name.begin(), record_name.end(), '.', ':'); 
-
-      
-      std::cout << "Record name: " << record_name << "\n";
-      
-      std::string type{boost::python::extract<std::string>(nodes[i].attr("typeStr"))}; 
-      std::cout << "Type: " << type << "\n"; 
-
-      if (type.compare("int") == 0) {
-        std::cout << "Found int type." << std::endl;
-	int value = boost::python::extract<int>(nodes[i].attr("value")); 
-	std::cout << "value: " << value << std::endl;
-      }
-
-      
-    }
-     	
-
-    //std::cout << base_class_name << std::endl;
+  std::cout << "Node count: " << nodes.size() << std::endl;
+  for (auto& [key, value] : nodes) { 
+      std::cout << "Node name: " << key << std::endl;
+      if (value.which() == 0) { 
+      	std::cout << "Node value (bool): " << boost::get<rogue::interfaces::api::Variable<bool>>(value).get() << std::endl;
+      } else if (value.which() == 1) { 
+      	std::cout << "Node value (int): " << boost::get<rogue::interfaces::api::Variable<int>>(value).get() << std::endl;
+      } else if (value.which() == 2) { 
+      	std::cout << "Node value (float): " << boost::get<rogue::interfaces::api::Variable<float>>(value).get() << std::endl;
+      } else if (value.which() == 3) { 
+      	std::cout << "Node value (uint8_t): " << boost::get<rogue::interfaces::api::Variable<uint8_t>>(value).get() << std::endl;
+      } else if (value.which() == 4) { 
+      	std::cout << "Node value (uint32_t): " << boost::get<rogue::interfaces::api::Variable<uint32_t>>(value).get() << std::endl;
+      } else if (value.which() == 5) { 
+      	std::cout << "Node value (uint64_t): " << boost::get<rogue::interfaces::api::Variable<uint64_t>>(value).get() << std::endl;
+      } else if (value.which() == 6) { 
+      	std::cout << "Node value (uintmax_t): " << boost::get<rogue::interfaces::api::Variable<uintmax_t>>(value).get() << std::endl;
+      } else if (value.which() == 7) { 
+      	std::cout << "Node value (std::string): " << boost::get<rogue::interfaces::api::Variable<std::string>>(value).get() << std::endl;
+      } 
   }
-  */
+
+  std::cout << "Local time: " << _root.getNode<rogue::interfaces::api::Variable<std::string>>("LocalTime").get() << std::endl;
 }
